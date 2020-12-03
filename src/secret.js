3e6853b996fd4a2611a26e0c93820388f3bdbffc
Jacqueline Egidio <jacquelineegidio@Jacquelines-MacBook-Pro.local>
2020-12-03 12:02:02 -0500

    Added Airtable key and base ID

--- src/secret.js
+++ src/secret.js
@@ -16,8 +16,8 @@ var getAPIusername = ""; // fill in with your name (if left blank, assuming usin
 // (2) Hardcode your API key information here
 
 // AIRTABLE SUPPORT
-var airtable_api = "key" + "XXXXXXXXXXX";     // fill in your info
-var airtable_baseid = "app" + "YYYYYYYYYYYY"; // fill in your info
+var airtable_api = "key" + "keyP1DtWtSXU95yGs";     // fill in your info
+var airtable_baseid = "app" + "appxDa2kew1z8g0V1"; // fill in your info
 
 // SYSTEMLINK SUPPORT
 var systemlink_api = "XXXXXXXXXXXXXXXXXX"; // fill in your info
